333355d8ee26da0d866619aeb86697787257ac60
GeorgeD <GeorgeD@CUBIC-1112>
2016-10-07 16:14:12 +0100

    Add project files.

---
+++ src/AspNetCoreMvc.Redis/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿namespace AspNetCoreMvc.Redis.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController: Controller
+    {
+         // GET /home/
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Welcome!";
+            return View();
+        }
+    }
+}
